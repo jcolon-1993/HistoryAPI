056868df3c12e7e715cd9796dfc189e7aecd8450
Jimmy Colon <60831920+jcolon-1993@users.noreply.github.com>
2021-06-21 16:32:06 -0700

    Create history.js

---
+++ js/history.js
@@ -0,0 +1,49 @@
+// DOM has loaded
+$(function ()
+{
+  // Load new content into page
+  function loadContent(url)
+  {
+    $("#content").load(url + " #container").hide().fadeIn("slow");
+  }
+
+  // Click handler
+  $("nav a").on("click", function (e)
+  {
+    // Stop link loading new page
+    e.preventDefault();
+
+    // Get href attribute of link
+    var href = this.href;
+
+    // Store link in jQuery object
+    var $this = $(this);
+
+    // Remove current from links
+    $("a").removeClass("current");
+
+    // Update current Link
+    $this.addClass("current");
+
+    // Call function: loads content
+    loadContent(href);
+
+    // Update History
+    history.pushState("", $this.text, href);
+  });
+   // handle back/foward buttons
+   window.onpopstate = function()
+   {
+     // Get the file path
+     var path = location.pathname;
+
+     // Call function to load page
+     loadContent(path);
+     var page = path.substring(location.pathname.lastIndexOf("/") + 1);
+
+     // Remove current from links
+     $("a").removeClass("current");
+     // update current
+     $("[href='" + page + "']").addClass("current");
+   };
+});
